18f352732ff49e350763abe7f7ca25307f03b305
0nrK <onurkirnapci@gmail.com>
2022-01-22 11:15:39 +0300

    change serviceImg's width

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Services = () => {
     return (
         <div >
-            <div className="flex flex-col flex-wrap md:space-y-0 space-y-4 p-4 grayscale md:flex-row justify-around align-center py-32">
+            <div className="flex flex-col flex-wrap md:space-y-0 md:space-x-5 space-y-4 p-4 grayscale md:flex-row justify-around align-center py-32">
                 <div className="relative cursor-pointer shadow-lg  flex flex-1  justify-center align-center text-center  ">
                     <img
                         className="object-cover h-96 md:h-auto"
@@ -14,9 +14,9 @@ const Services = () => {
                         <span className="text-white text-l">SEE PROJECTS</span>
                     </div>
                 </div>
-                <div className="relative flex flex-1 drop-shadow-lg  justify-center align-center text-center h-96 cursor-pointer">
+                <div className="relative w-full md:flex-1 flex drop-shadow-lg  justify-center align-center text-center h-96 cursor-pointer">
                     <img
-                        className="object-cover h-96 md:h-auto"
+                        className="object-cover h-96 w-full  md:h-auto"
                         alt=""
                         src="https://images.unsplash.com/photo-1561070791-2526d30994b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" />
                     <div className="absolute top-1/2 ml-auto text-center">
